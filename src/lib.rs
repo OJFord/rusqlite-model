bb08560f444f281abfbcc0e39e515b867c2cea1f
Oliver Ford <dev@ojford.com>
2021-02-27 20:14:24 +0000

    Construct model from query

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,7 @@ use syn::DeriveInput;
 #[proc_macro_derive(Model)]
 pub fn derive_model(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    impl_model(&ast)
-}
 
-fn impl_model(ast: &DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let fields = match &ast.data {
         syn::Data::Struct(data) => match &data.fields {
@@ -20,17 +17,42 @@ fn impl_model(ast: &DeriveInput) -> TokenStream {
             _ => panic!("expected named fields"),
         },
         _ => panic!("expected struct"),
-    };
+    }
+    .collect();
 
+    let modelimpl = impl_model(&name, &fields);
+    let tryfromrow = impl_tryfrom(&fields);
+
+    let mut ts = TokenStream::new();
+    ts.extend(modelimpl);
+    ts.extend(tryfromrow);
+    ts
+}
+
+fn impl_model(name: &syn::Ident, fields: &Vec<&syn::Ident>) -> TokenStream {
     let gen = quote! {
-
-
-      impl Model for #name {
+      impl Model<'_> for #name {
         fn into_params(self) -> std::vec::IntoIter<Box<dyn rusqlite::ToSql>> {
           let ret: Vec<Box<dyn rusqlite::ToSql>> = vec![
             #(Box::new(self.#fields),)*
           ];
           ret.into_iter()
+        }
+      }
+    };
+
+    gen.into()
+}
+
+fn impl_tryfrom(fields: &Vec<&syn::Ident>) -> TokenStream {
+    let gen = quote! {
+      impl<'a> std::convert::TryFrom<&'a rusqlite::Row<'a>> for Transaction {
+        type Error = rusqlite::Error;
+
+        fn try_from(row: &rusqlite::Row<'_>) -> rusqlite::Result<Self> {
+          Ok(Self {
+            #(#fields: row.get(row.column_index(std::stringify!(#fields))?)?,)*
+          })
         }
       }
     };
