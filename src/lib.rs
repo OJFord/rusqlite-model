c520f8a554f86cae6053952b2e5b4c0232dbeb6b
Oliver Ford <dev@ojford.com>
2021-02-28 17:15:52 +0000

    Derive a create_table fn

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,15 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse_macro_input;
 use syn::DeriveInput;
-use syn::Ident;
 
-#[proc_macro_derive(Model)]
+#[proc_macro_derive(Model, attributes(sql_type))]
 pub fn derive_model(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
     let name = &ast.ident;
     let fields = match &ast.data {
         syn::Data::Struct(data) => match &data.fields {
-            syn::Fields::Named(fields) => fields
-                .named
-                .iter()
-                .map(|f| f.ident.as_ref().expect("expected named fields")),
+            syn::Fields::Named(fields) => fields.named.iter(),
             _ => panic!("expected named fields"),
         },
         _ => panic!("expected struct"),
@@ -30,17 +26,62 @@ pub fn derive_model(input: TokenStream) -> TokenStream {
     ts
 }
 
-fn impl_model(name: &Ident, fields: &Vec<&Ident>) -> TokenStream {
+fn impl_model(name: &syn::Ident, fields: &Vec<&syn::Field>) -> TokenStream {
     let param_holders = vec![quote!(?); fields.len()];
     let mut table_name = format!("{}s", name);
     table_name.make_ascii_lowercase();
 
+    let field_names: Vec<_> = fields
+        .iter()
+        .map(|f| f.ident.as_ref().expect("expected named field"))
+        .collect();
+
+    let sql_types = fields.iter().map(|f| {
+        for attr in &f.attrs {
+            if let Some(attrident) = attr.path.get_ident() {
+                if attrident.to_string() == "sql_type" {
+                    return attr.parse_args().expect("failed to read sql_type");
+                }
+            }
+        }
+
+        match &f.ty {
+            syn::Type::Path(tp) => match tp.path.get_ident() {
+                Some(ident) => match ident.to_string().as_ref() {
+                    "bool" => quote!(BOOL NOT NULL),
+                    "String" => quote!(TEXT NOT NULL),
+                    _ => panic!("Don't know how to map to SQL type {}", ident.to_string()),
+                },
+                None => panic!("Unsupported type path"),
+            },
+            _ => panic!("Don't know how to map to SQL type {:?}", f.ty),
+        }
+    });
+
     let gen = quote! {
       impl Model<'_> for #name {
 
+        fn create_table(conn: &rusqlite::Connection) -> rusqlite::Result<usize> {
+          conn.execute(std::stringify!(
+              CREATE TABLE #table_name (
+                #(#field_names #sql_types),*
+              )
+            ),
+            rusqlite::NO_PARAMS,
+          )
+        }
+
+        fn drop_table(conn: &rusqlite::Connection) -> rusqlite::Result<usize> {
+          conn.execute(std::stringify!(
+              DROP TABLE IF EXISTS #table_name
+            ),
+            rusqlite::NO_PARAMS,
+          )
+        }
+
         fn insert(self, conn: &rusqlite::Connection) -> rusqlite::Result<usize> {
           let mut stmt = conn
-            .prepare(std::stringify!(INSERT INTO #table_name (#(#fields),*) VALUES (#(#param_holders),*)))
+            .prepare(std::stringify!(INSERT INTO #table_name (#(#field_names),*) VALUES (#(#param_holders),*)))
             .unwrap();
 
           println!("{:?}", stmt);
@@ -49,7 +90,7 @@ fn impl_model(name: &Ident, fields: &Vec<&Ident>) -> TokenStream {
 
         fn into_params(self) -> std::vec::IntoIter<Box<dyn rusqlite::ToSql>> {
           let ret: Vec<Box<dyn rusqlite::ToSql>> = vec![
-            #(Box::new(self.#fields),)*
+            #(Box::new(self.#field_names),)*
           ];
           ret.into_iter()
         }
@@ -60,14 +101,18 @@ fn impl_model(name: &Ident, fields: &Vec<&Ident>) -> TokenStream {
     gen.into()
 }
 
-fn impl_tryfrom(fields: &Vec<&syn::Ident>) -> TokenStream {
+fn impl_tryfrom(fields: &Vec<&syn::Field>) -> TokenStream {
+    let field_names = fields
+        .iter()
+        .map(|f| f.ident.as_ref().expect("expected named field"));
+
     let gen = quote! {
       impl<'a> std::convert::TryFrom<&'a rusqlite::Row<'a>> for Transaction {
         type Error = rusqlite::Error;
 
         fn try_from(row: &rusqlite::Row<'_>) -> rusqlite::Result<Self> {
           Ok(Self {
-            #(#fields: row.get(row.column_index(std::stringify!(#fields))?)?,)*
+            #(#field_names: row.get(row.column_index(std::stringify!(#field_names))?)?,)*
           })
         }
       }
